f781f3adf720b959175d8cf0b10ff1fc16727e43
Thato Mothusi <ThatoM@TheNSG.gov.za>
2021-07-09 13:52:56 +0200

    updated project structure

---
+++ Public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8" >
+    <title>Global bank </title>
+</head>
+<body>
+    <h1>Global Bank:Coming Soon</h1>
+</body>
+</html>
---
+++ Public/staff/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>GBI</title>
+        <link rel="stylesheet" href="../stylesheets/style.css" type="text/css">
+    </head>
+
+<body>
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php" target="_blank">Menu</a></li>
+        </ul>
+    </navigation>
+    <footer>
+        &copy; <?php echo date('Y');?>
+    </footer>
+</body>
+
+</html>
